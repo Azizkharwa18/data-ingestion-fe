048358f9ccc65bbe10208700953e447a06e93ebd
Azizkharwa18 <azizkharwawala@gmail.com>
2025-12-11 19:41:38 +0530

    New CHanges

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,56 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { timer, switchMap, takeWhile, finalize } from 'rxjs';
+import { DataService } from './services/data-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('distributed-processing-fe');
+  private dataService = inject(DataService);
+
+  isLoading = signal(false);
+  statusMessage = signal('');
+  finalResult = signal<any>(null);
+
+  triggerPipeline() {
+    this.isLoading.set(true);
+    this.statusMessage.set('Requesting task...');
+    this.finalResult.set(null);
+
+    this.dataService.startProcessing().subscribe({
+      next: (resp) => {
+        this.statusMessage.set(`Task Started (ID: ${resp.task_id}). Waiting for worker...`);
+        this.pollStatus(resp.task_id);
+      },
+      error: (err) => {
+        this.statusMessage.set('Error starting task.');
+        this.isLoading.set(false);
+      }
+    });
+  }
+
+  pollStatus(taskId: string) {
+    // Poll every 2 seconds until status is SUCCESS or FAILURE
+    timer(0, 2000)
+      .pipe(
+        switchMap(() => this.dataService.checkStatus(taskId)),
+        takeWhile(res => res.status !== 'SUCCESS' && res.status !== 'FAILURE', true),
+        finalize(() => this.isLoading.set(false))
+      )
+      .subscribe((res) => {
+        this.statusMessage.set(`Current State: ${res.status}`);
+
+        if (res.status === 'SUCCESS') {
+          this.finalResult.set(res.result);
+        } else if (res.status === 'FAILURE') {
+          this.statusMessage.set('Pipeline Failed.');
+        }
+      });
+  }
 }
---
+++ src/app/services/data-service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface TaskResponse {
+  task_id: string;
+  status: string;
+  result?: any;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:5000/api';
+
+  startProcessing(): Observable<{ task_id: string }> {
+    return this.http.post<{ task_id: string }>(`${this.apiUrl}/start-task`, {
+      filename: 'large_sales_data.csv'
+    });
+  }
+
+  checkStatus(taskId: string): Observable<TaskResponse> {
+    return this.http.get<TaskResponse>(`${this.apiUrl}/status/${taskId}`);
+  }
+}
